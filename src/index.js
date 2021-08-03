351f9bd9566e9c700444163c39304bc0a430870f
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-08-03 13:46:52 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function addToys(name, image) {
+  let formData = {
+    name: name,
+    image: image
+  };
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/toys", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+
+  })
+}
+
+
+
+// define a function that fetches all the toy objects
+// with the response data, make a <div class="card"> for each toy and add it to the toy-collection div
+// each card should have h2 tag with the toy's name 
+// img tag with the src of the toy's image attribute and the class name "toy-avatar" 
+// p tag with how many likes that toy has
+// button tag with a class "like-btn"
+// call this function under DOM content loaded event listener
